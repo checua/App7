4221451f12f1a4abef65f7d48dccf3230453fb4c
checua <profesor76@hotmail.com>
2021-09-01 21:23:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/app7/ConnectionHelper.java
@@ -0,0 +1,41 @@
+package com.example.app7;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper {
+
+
+    @SuppressLint("NewApi")
+    public static Connection CONN() {
+
+        String _user = "misiones";
+        String _pass = "Alopedev7mision";
+        String _DB = "mapsMarkers";
+        String _server = "dpa6ecad46.database.windows.net";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnURL = "jdbc:jtds:sqlserver://" + _server + ";"
+                    + "databaseName=" + _DB + ";user=" + _user + ";password="
+                    + _pass + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
